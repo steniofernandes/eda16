f6c7fe5c2a7d75082853e49d8e0713ab360a8321
steniofernandes <steniofernandes@gmail.com>
2016-01-24 16:32:17 -0500

    Small corrections on plot4.r

--- plot4.r
+++ plot4.r
@@ -7,17 +7,18 @@ ss_dates <- as.Date(hpc$Date, "%d/%m/%Y")
 ss_hpc <- data.frame(hpc[which(ss_dates>="2007-02-01" & ss_dates<="2007-02-02"),])
 
 par(mfcol=c(2,2))
+par(mar = c(4,4,1,1))
 #1
 plot(ss_hpc$Time, ss_hpc$Global_active_power,  
        type="l", 
        col = "black",
-       ylab = "GAP (kilowatts)")
+       ylab = "GAP (kilowatts)", xlab="")
        
 #2
 plot(ss_hpc$Time, ss_hpc$Sub_metering_1, 
      type="l", 
      col = "black",
-     ylab = "Energy Sub metering")
+     ylab = "Energy Sub metering", xlab="")
 lines(ss_hpc$Sub_metering_2, col = "red")
 lines(ss_hpc$Sub_metering_3, col = "blue")
 
@@ -25,13 +26,13 @@ lines(ss_hpc$Sub_metering_3, col = "blue")
 plot(ss_hpc$Time, ss_hpc$Voltage,  
      type="l", 
      col = "black",
-     ylab = "Voltage")
+     ylab = "Voltage", xlab="")
 
 #4
 plot(ss_hpc$Time, ss_hpc$Global_reactive_power,  
      type="l", 
      col = "black",
-     ylab = "GRP")
+     ylab = "GRP", xlab="")
 
 
 dev.copy(png, file = "plot4.png") ## Copy plot4 to a PNG file
